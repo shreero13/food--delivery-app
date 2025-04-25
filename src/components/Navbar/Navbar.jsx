5de232b2f046110aefcfcbe7c15e72a859f96bf7
shreero13 <subashreemoorthy2@gmail.com>
2025-04-25 15:05:23 +0530

    FIX

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 import  './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
-import { StoreContext } from '../../context/StoreContext.jsx';
+import { StoreContext } from '../../Context/StoreContext.jsx';
 const Navbar = ({setShowLogin}) => {
 
 const [menu,setMenu] = useState("menu");
