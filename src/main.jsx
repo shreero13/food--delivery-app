0a58e7c369a2a0703e528086eba37a0aedb0f003
shreero13 <subashreemoorthy2@gmail.com>
2025-04-25 11:10:20 +0530

    Fix: correct StoreContext import casing

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import StoreContextProvider from "./context/Storecontext.jsx";
-import { StoreContext } from "./context/Storecontext.jsx";
+import StoreContextProvider from "./context/StoreContext.jsx";
+
 
 
 createRoot(document.getElementById("root")).render(
