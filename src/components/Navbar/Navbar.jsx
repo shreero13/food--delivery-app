78bf521860619156244d97fecaca87b75b978572
shreero13 <subashreemoorthy2@gmail.com>
2025-04-25 11:38:27 +0530

    Fix: correct StoreContext import path in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,7 @@ import React, { useContext, useState } from 'react'
 import  './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom';
-import { StoreContext } from "../../context/Storecontext";
-
+import { StoreContext } from '../../context/StoreContext.jsx';
 const Navbar = ({setShowLogin}) => {
 
 const [menu,setMenu] = useState("menu");
